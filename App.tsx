f0d2ecb521e722b18ca51d88871e5c2bc8a15586
Alec Diaz <alecdiaz1@yahoo.com>
2020-07-06 10:52:46 -0700

    wrap app with redux store

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import {store} from "./redux/store";
+import {Provider} from "react-redux";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,8 +17,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <Provider store={store}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </Provider>
       </SafeAreaProvider>
     );
   }
