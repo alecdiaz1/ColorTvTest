ff257e92f606532b4b44ebc641e2a9e23a943e55
Alec Diaz <alecdiaz1@yahoo.com>
2020-07-06 10:54:20 -0700

    fetch users from search term

--- redux/modules/usersSlice.ts
+++ redux/modules/usersSlice.ts
@@ -1,37 +1,46 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { API_BASE_URL } from '../../env.json';
+import {getAccessToken, getData} from '../../services/apiService';
 
 const initialState = {
-    loading: false,
-    users: [],
-    error: ''
+  loading: false,
+  users: [],
+  error: '',
 };
 
 const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-        fetchUsersBegin(state) {
-            state.loading = true;
-        },
-        fetchUsersSuccess(state, action) {
-            state.loading = false;
-            state.users = action.payload;
-            state.error = '';
-        },
-        fetchUsersFailure(state, action) {
-            state.loading = false;
-            state.error = action.payload
-        }
-    }
-})
+  name: 'users',
+  initialState,
+  reducers: {
+    fetchUsersBegin(state) {
+      state.loading = true;
+    },
+    fetchUsersSuccess(state, action) {
+      state.loading = false;
+      state.users = action.payload;
+      state.error = '';
+    },
+    fetchUsersFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
 
-export const { fetchUsersBegin, fetchUsersSuccess, fetchUsersFailure } = usersSlice.actions
+export const { fetchUsersBegin, fetchUsersSuccess, fetchUsersFailure } = usersSlice.actions;
+export default usersSlice.reducer;
 
-export const searchUsers = (searchTerm: string) => {
-    return function(dispatch: any) {
-        dispatch(fetchUsersBegin());
-        getData(`${API_BASE_URL}/search/users?page=1&query=${searchTerm}`)
-            .then(response => dispatch(fetchUsersSuccess(response.results)))
-    }
-}
+export const searchUsers = (searchTerm: string) => function (dispatch: any) {
+  dispatch(fetchUsersBegin());
+  getData(`${API_BASE_URL}/search/users?page=1&query=${searchTerm}`)
+    .then((response) => {
+      if (response.errors) {
+        dispatch(fetchUsersFailure(response.errors[0]));
+      } else {
+        dispatch(fetchUsersSuccess(response.results));
+      }
+    })
+    .catch((error) => {
+      dispatch(fetchUsersFailure(error));
+    });
+};
